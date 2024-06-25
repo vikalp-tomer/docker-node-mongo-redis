2584837c6b2385c788a34cee1f70199630ca04a3
vikalp-tomer <vikalptomar15@gmail.com>
2024-06-25 21:19:10 +0530

    added docker compose for dev and prod

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!!!");
+  res.send("Hello World!!!");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
